29f3c5e31d7cb9a5cf811cdb4e6229385606a032
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2025-03-11 09:05:25 -0300

    Add files via upload

---
+++ FISS.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name          Checagem dos FIIS
+// @description   Abre as páginas dos FIIS ao pressionar Shift + 1 e Shift + 2
+// @namespace     CowanFISS
+// @license       CowBas
+// @version       1.0
+// @author        Cowanbas
+// @match         *://*/*
+// @run-at        document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const links = {
+        'Digit1': 'https://investidor10.com.br/fiis/mxrf11/',
+        'Digit2': 'https://investidor10.com.br/fiis/gare11/'
+    };
+
+    document.addEventListener('keydown', function(event) {
+        if (event.shiftKey && links[event.code] && !event.target.matches('input, textarea, [contenteditable="true"]')) {
+            window.open(links[event.code], '_blank');
+        }
+    });
+})();
